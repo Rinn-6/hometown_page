787a68d74edfce4b6c4fa973e73b82c2b351647f
Payal Kumari <122254002+Rinn-6@users.noreply.github.com>
2023-03-29 10:22:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import Activities from './components/Activities'
 import Guide from './components/Guide'
+import './assets'
 
 const App = () => {
    
